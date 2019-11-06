450221a16eba1236f7b208166f38f1777be4d93b
Alexander Kudlai <student@kudlai.com>
2019-11-06 23:33:58 +0200

    Started Add Book model refactoring.

--- BookCatalog.Web/Scripts/AppScripts/BookCatalog/book-catalog-controller.ts
+++ BookCatalog.Web/Scripts/AppScripts/BookCatalog/book-catalog-controller.ts
@@ -1,4 +1,5 @@
-﻿///<reference path="book-catalog-business.ts" /> 
+﻿///<reference path="book-catalog-business.ts" />
+///<reference path="../../typings/jquery/jquery.d.ts" />
 
 class BookCatalogController {
 
@@ -11,4 +12,11 @@ class BookCatalogController {
             ko.applyBindings(this.business.getModel());
         }
     }
+
+    public addBook = (): void => {
+        // To-Do add validation.
+        this.business.addBook();
+    }
+
+
 }
